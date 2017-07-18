a4b0b8173dcc8067d45249c4ef53b9de1320c7fe
georgiasalanti <georgia.salanti@gmail.com>
2017-07-18 14:31:40 +0200

    Correcting beta
    chagned b[1] to b[ref]

--- R/modelNMRContinuous.R
+++ R/modelNMRContinuous.R
@@ -1,30 +1,30 @@
 modelNMRContinuous=
-function () 
+function ()
 {
   for (i in 1:ns) {
     w[i, 1] <- 0
     delta[i, t[i, 1]] <- 0
     u[i] ~ dnorm(0, 1e-04)
     for (k in 1:na[i]) {
-      y[i, t[i, k]] ~ dnorm(phi[i, t[i, k]], prec[i, t[i, 
+      y[i, t[i, k]] ~ dnorm(phi[i, t[i, k]], prec[i, t[i,
                                                        k]])
       phi[i, t[i, k]] <- (u[i] + delta1[i, t[i, k]]) * pooled.sd[i]
       delta1[i, t[i, k]]<- delta[i,t[i,k]]+beta[t[i,1],t[i,k]]*variab[i]
     }
     for (k in 2:na[i]) {
-      
-      delta[i, t[i, k]] ~ dnorm(md[i, t[i, k]], taud[i, 
+
+      delta[i, t[i, k]] ~ dnorm(md[i, t[i, k]], taud[i,
                                                      t[i, k]])
-      md[i, t[i, k]] <- d[t[i, k]] - d[t[i, 1]] + sw[i, 
+      md[i, t[i, k]] <- d[t[i, k]] - d[t[i, 1]] + sw[i,
                                                      k]
       taud[i, t[i, k]] <- PREC * 2 * (k - 1)/k
-      w[i, k] <- (delta[i, t[i, k]] - d[t[i, k]] + d[t[i, 
+      w[i, k] <- (delta[i, t[i, k]] - d[t[i, k]] + d[t[i,
                                                        1]])
       sw[i, k] <- sum(w[i, 1:(k - 1)])/(k - 1)
     }
   }
   d[ref] <- 0
-  
+
   for (k in 1:(ref - 1)) {
     d[k] ~ dnorm(0, 1e-04)
   }
@@ -67,16 +67,20 @@ function ()
     }
   }
   for (k in 1:nt) {
-    SUCRA[k] <- sum(cumeffectiveness[k, 1:(nt - 1)])/(nt - 
+    SUCRA[k] <- sum(cumeffectiveness[k, 1:(nt - 1)])/(nt -
                                                         1)
   }
-  
+
   #regression coefficients
-  b[1]<-0
-  for(k in 2:nt) {
-    
+  b[ref]<-0
+  for(k in 1:(ref-1)) {
     b[k] ~ dnorm(0,0.0001)
   }
+  for(k in (ref+1):nt) {
+    b[k] ~ dnorm(0,0.0001)
+  }
+
+
   for(i in 1:nt) {
     for (j in 1:nt) {
       beta[i,j]<-b[j]-b[i]
